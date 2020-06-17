622f5f4ca0409f2380b0e44c837fca3c06375414
Nolm <nolm88@yandex.ru>
2020-06-17 08:44:49 +0300

    redy

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,30 @@
 const popup = document.querySelector('.popup');
-const popupOpenButton = document.querySelector('.profile__open-popup');
+const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = popup.querySelector('.popup__close');
+const popupSubmitButton = popup.querySelector('.popup__submit-button');
+let nameInput = document.querySelector('.popup__item_name');
+let jobInput = document.querySelector('.popup__item_job');
+let userTitle = document.querySelector('.profile__title');
+let userSubtitle = document.querySelector('.profile__subtitle');
 
+//открытие и закрытие попап//
 const popupToggle = function() {
   popup.classList.toggle('popup_opened')
+  nameInput.value = userTitle.textContent;
+  jobInput.value = userSubtitle.textContent;
 }
 
-popupOpenButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
-
-let nameInput = document.querySelector('.popup__item-name');
-let jobInput = document.querySelector('.popup__item-job');
-let userTitle = document.querySelector('.profile__title-user');
-let userSubtitle = document.querySelector('.profile__subtitle-user');
-nameInput.value = userTitle.textContent;
-jobInput.value = userSubtitle.textContent;
-
-
+//изменение данных на странице через форму//
 let formElement = document.querySelector('.popup__form');
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let nameInput = document.querySelector('.popup__item-name');
-    let jobInput = document.querySelector('.popup__item-job');
     userTitle.textContent = nameInput.value;
     userSubtitle.textContent = jobInput.value;
+    popupSubmitButton.addEventListener('click', popupToggle);
 }
 
+//будет отправка формы//
 formElement.addEventListener('submit', formSubmitHandler);
+
+popupOpenButton.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
